fb85ad2d5e9972215c860f77bacb0bd6d3ce3f20
[Carlos Galaron] <[carlos.galaron@gmail.com]>
2024-10-29 14:18:57 +0100

    Componente de modo oscuro

---
+++ src/lib/ToggleDarkMode.svelte
@@ -0,0 +1,35 @@
+<script>
+    let isDarkMode=false
+    const toggleTheme= () => {
+        isDarkMode = !isDarkMode
+    }
+        
+</script>
+
+<div
+    class:dark-mode={isDarkMode}
+    class:light-mode={!isDarkMode}>
+    <h1>
+        I change my color
+    </h1>
+</div>
+    <button on:click={toggleTheme}>Toggle</button>
+
+<style>
+    .light-mode {
+        color: black;
+        background-color: white;
+    }
+    .dark-mode {
+        color: white;
+        background-color: black;
+    }
+
+    button{
+        width: 100px;
+        height: 60px;
+        background-color: red;
+    }
+
+
+</style>
